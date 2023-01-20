a89ca9c805972e3ff936ba52badcaaa5995e8404
Son Do <dominhson580@gmail.com>
2023-01-20 15:31:37 +0700

    Add links to different photo pages

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate, Link } from "react-router-dom";
 
 import Photo from "./features/photo";
 import NotFound from "./components/NotFound";
@@ -9,8 +9,20 @@ function App() {
   return (
     <div className="photo-app">
       <BrowserRouter>
+        <ul>
+          <li>
+            <Link to="/photos">Main Photo Page</Link>
+          </li>
+          <li>
+            <Link to="/photos/add">Add New Photo Page</Link>
+          </li>
+          <li>
+            <Link to="/photos/:id">Edit Photo Page</Link>
+          </li>
+        </ul>
+
         <Routes>
-          <Route path="/" element={<Navigate to="/photos" />} />
+          <Route path="/" loader={<Navigate to="/photos" />} />
           <Route path="/photos" element={<Photo />} />
           <Route path="*" element={<NotFound />} />
         </Routes>
