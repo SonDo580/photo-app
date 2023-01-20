571cb7336f5225d617360da837594ac80970fc02
Son Do <dominhson580@gmail.com>
2023-01-20 16:41:15 +0700

    Define Header component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,32 @@
+import { NavLink } from "react-router-dom";
+import { Col, Container, Row } from "reactstrap";
+
 function Header() {
-  return <div>Header</div>;
+  return (
+    <header>
+      <Container>
+        <Row className="justify-content-between">
+          <Row>
+            <Col xs="auto">
+              <a
+                href="https://youtube.com/easyfrontend"
+                target="_blank"
+                rel="noreferrer"
+                className="header-link header-title"
+              >
+                Easy Frontend
+              </a>
+            </Col>
+            <Col xs="auto">
+              <NavLink to="/photos" className="header-link">
+                Redux Project
+              </NavLink>
+            </Col>
+          </Row>
+        </Row>
+      </Container>
+    </header>
+  );
 }
 
 export default Header;
