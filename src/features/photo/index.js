778bab999419f28d428a2d0a9cdc0b3d6485dd96
Son Do <dominhson580@gmail.com>
2023-01-20 15:47:08 +0700

    Define routes for 'photos' path

--- src/features/photo/index.js
+++ src/features/photo/index.js
@@ -1,5 +1,17 @@
+import { Routes, Route } from "react-router-dom";
+
+import Main from "./pages/Main";
+import Add from "./pages/Add";
+import Edit from "./pages/Edit";
+
 function Photo() {
-  return <div>Photo</div>;
+  return (
+    <Routes>
+      <Route path="/" element={<Main />} />
+      <Route path="/add" element={<Add />} />
+      <Route path="/:id" element={<Edit />} />
+    </Routes>
+  );
 }
 
 export default Photo;
