515d10e89b24fc3ec4412f9203e23cbcc606a198
Son Do <dominhson580@gmail.com>
2023-01-21 23:25:48 +0700

    Change constant name

--- src/constants/global.js
+++ src/constants/global.js
@@ -1,4 +1,4 @@
-export const PHOTO_CATEGORY_OPTIONS = [
+export const PHOTO_CATEGORIES = [
   { value: 1, label: "Technology" },
   { value: 2, label: "Education" },
   { value: 3, label: "Nature" },
