388d27eb018818a6d939a5bed1f5482289cced23
Son Do <dominhson580@gmail.com>
2023-01-21 14:30:57 +0700

    Remove redundant row

--- src/components/Header.js
+++ src/components/Header.js
@@ -8,23 +8,21 @@ function Header() {
     <header className="header">
       <Container>
         <Row className="justify-content-between">
-          <Row>
-            <Col xs="auto">
-              <a
-                href="https://youtube.com/easyfrontend"
-                target="_blank"
-                rel="noreferrer"
-                className="header-link header-title"
-              >
-                Easy Frontend
-              </a>
-            </Col>
-            <Col xs="auto">
-              <NavLink to="/photos" className="header-link">
-                Redux Project
-              </NavLink>
-            </Col>
-          </Row>
+          <Col xs="auto">
+            <a
+              href="https://youtube.com/easyfrontend"
+              target="_blank"
+              rel="noreferrer"
+              className="header-link header-title"
+            >
+              Easy Frontend
+            </a>
+          </Col>
+          <Col xs="auto">
+            <NavLink to="/photos" className="header-link">
+              Redux Project
+            </NavLink>
+          </Col>
         </Row>
       </Container>
     </header>
