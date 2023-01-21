70629ac9f88d644eb17ee6661670d2f19c36c0c1
Son Do <dominhson580@gmail.com>
2023-01-21 15:00:22 +0700

    Add a banner to photo main page

--- src/features/photo/pages/Main.js
+++ src/features/photo/pages/Main.js
@@ -1,5 +1,12 @@
+import Banner from "../../../components/Banner";
+import Images from "../../../constants/images";
+
 function Main() {
-  return <div>Photo Main Page</div>;
+  return (
+    <div className="photo-main">
+      <Banner title="Your awesome photos" backgroundUrl={Images.PINK_BG} />
+    </div>
+  );
 }
 
 export default Main;
