311295289479879bd756ebdcdecc6084e905a3bb
Son Do <dominhson580@gmail.com>
2023-01-21 14:55:15 +0700

    Use constants to store image urls

---
+++ src/constants/images.js
@@ -0,0 +1,11 @@
+import ORANGE_BG from "../assets/images/orange-bg.jpg";
+import PINK_BG from "../assets/images/pink-bg.jpg";
+import WHITE_BG from "../assets/images/white-bg.jpg";
+
+const Images = {
+  ORANGE_BG,
+  PINK_BG,
+  WHITE_BG,
+};
+
+export default Images;
