db0595b563d6b95e6b42d9cc95020cfb3781a958
Son Do <dominhson580@gmail.com>
2023-01-22 15:56:52 +0700

    Define InputField

--- src/features/photo/components/InputField.js
+++ src/features/photo/components/InputField.js
@@ -1,5 +1,20 @@
-function InputField() {
-  return <div>InputField</div>;
+import { FormGroup, Input, Label } from "reactstrap";
+
+function InputField(props) {
+  console.log(props);
+  const { field, type, label, disabled } = props;
+
+  return (
+    <FormGroup>
+      <Label for={field.name}>{label}</Label>
+      <Input
+        id={field.name}
+        type={type ? type : "text"}
+        disabled={disabled}
+        {...field}
+      />
+    </FormGroup>
+  );
 }
 
 export default InputField;
