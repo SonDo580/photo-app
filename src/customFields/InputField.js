7036e718bae0cb6859d3737a48012c37df6d3c2b
Son Do <dominhson580@gmail.com>
2023-01-22 16:03:21 +0700

    Move InputField to customFields folder

---
+++ src/customFields/InputField.js
@@ -0,0 +1,20 @@
+import { FormGroup, Input, Label } from "reactstrap";
+
+function InputField(props) {
+  console.log(props);
+  const { field, type, label, disabled } = props;
+
+  return (
+    <FormGroup>
+      <Label for={field.name}>{label}</Label>
+      <Input
+        id={field.name}
+        type={type ? type : "text"}
+        disabled={disabled}
+        {...field}
+      />
+    </FormGroup>
+  );
+}
+
+export default InputField;
