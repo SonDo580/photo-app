89a4893f1018937aa8b02c0e975eccd467f84823
Son Do <dominhson580@gmail.com>
2023-01-22 15:39:23 +0700

    Use Formik component

--- src/features/photo/components/PhotoForm.js
+++ src/features/photo/components/PhotoForm.js
@@ -1,43 +1,65 @@
-import { Button, Form, FormGroup, Input, Label } from "reactstrap";
+import { Button, FormGroup, Input, Label } from "reactstrap";
 import Select from "react-select";
 
 import { PHOTO_CATEGORIES } from "constants/global";
 import Images from "constants/images";
+import { FastField, Form, Formik } from "formik";
+import InputField from "./InputField";
 
 function PhotoForm() {
   return (
-    <Form>
-      <FormGroup>
-        <Label for="title">Title</Label>
-        <Input name="title" id="title" />
-      </FormGroup>
+    <Formik>
+      {(formikProps) => {
+        const { values, errors, touched } = formikProps;
+        console.log(formikProps);
 
-      <FormGroup>
-        <Label for="category">Category</Label>
-        <Select id="category" name="category" options={PHOTO_CATEGORIES} />
-      </FormGroup>
+        return (
+          <Form>
+            <FastField
+              name="title"
+              component={InputField}
+              type="text"
+              label="Title"
+            />
 
-      <FormGroup>
-        <Label for="photo">Photo</Label>
-        <div>
-          <Button outline color="primary">
-            Random a Photo
-          </Button>
-        </div>
-        <div>
-          <img
-            width="200px"
-            height="200px"
-            src={Images.ORANGE_BG}
-            alt="default"
-          />
-        </div>
-      </FormGroup>
+            <FormGroup>
+              <Label for="title">Title</Label>
+              <Input name="title" id="title" />
+            </FormGroup>
 
-      <FormGroup>
-        <Button color="primary">Add to album</Button>
-      </FormGroup>
-    </Form>
+            <FormGroup>
+              <Label for="category">Category</Label>
+              <Select
+                id="category"
+                name="category"
+                options={PHOTO_CATEGORIES}
+              />
+            </FormGroup>
+
+            <FormGroup>
+              <Label for="photo">Photo</Label>
+              <div>
+                <Button outline color="primary">
+                  Random a Photo
+                </Button>
+              </div>
+              <div>
+                <img
+                  width="200px"
+                  height="200px"
+                  src={Images.ORANGE_BG}
+                  alt="default"
+                />
+              </div>
+            </FormGroup>
+
+            <FormGroup>
+              <Button color="primary">Add to album</Button>
+            </FormGroup>
+          </Form>
+        );
+      }}
+    </Formik>
   );
 }
 
