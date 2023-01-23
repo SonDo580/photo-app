50a0bc92a5e4b62e2080d019a82f779457792173
Son Do <dominhson580@gmail.com>
2023-01-23 23:00:53 +0700

    Add random photo functionality

--- src/components/RandomPhoto.js
+++ src/components/RandomPhoto.js
@@ -1,12 +1,28 @@
 import { Button } from "reactstrap";
 
 function RandomPhoto(props) {
-  const { name, imageUrl, onRandomButtonBlur } = props;
+  const { name, imageUrl, onRandomButtonBlur, onImageUrlChange } = props;
+
+  const getRandomImageUrl = () => {
+    const randomID = Math.trunc(Math.random() * 1000);
+    return `https://picsum.photos/id/${randomID}/300/300`;
+  };
+
+  const handleRandomPhoto = () => {
+    const randomImageUrl = getRandomImageUrl();
+    onImageUrlChange(randomImageUrl);
+  };
 
   return (
     <div className="random-photo">
       <div className="random-photo__buton">
-        <Button outline color="primary" name={name} onBlur={onRandomButtonBlur}>
+        <Button
+          outline
+          color="primary"
+          name={name}
+          onBlur={onRandomButtonBlur}
+          onClick={handleRandomPhoto}
+        >
           Random a Photo
         </Button>
       </div>
