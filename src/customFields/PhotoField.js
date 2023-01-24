29e086f6e46bde86e76ae9a9ac5625191248cec7
Son Do <dominhson580@gmail.com>
2023-01-24 14:53:37 +0700

    Show error

--- src/customFields/PhotoField.js
+++ src/customFields/PhotoField.js
@@ -1,9 +1,14 @@
 import RandomPhoto from "components/RandomPhoto";
-import { FormGroup, Label } from "reactstrap";
+
+import { ErrorMessage } from "formik";
+import { FormFeedback, FormGroup, Label } from "reactstrap";
 
 function PhotoField(props) {
   const { field, form, label } = props;
   const { name, value, onBlur } = field;
+  const { errors, touched } = form;
+
+  const showError = errors[name] && touched[name];
 
   const handleImageUrlChange = (newImageUrl) => {
     form.setFieldValue(name, newImageUrl);
@@ -18,6 +23,9 @@ function PhotoField(props) {
         onRandomButtonBlur={onBlur}
         onImageUrlChange={handleImageUrlChange}
       />
+
+      <div className={showError ? "is-invalid" : ""}></div>
+      <ErrorMessage name={name} component={FormFeedback} />
     </FormGroup>
   );
 }
