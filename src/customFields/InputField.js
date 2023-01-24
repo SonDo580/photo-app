9a8fe8b7024bc39b4fb6ad2ee2d42c3d119f2797
Son Do <dominhson580@gmail.com>
2023-01-24 14:19:44 +0700

    Show error in InputField

--- src/customFields/InputField.js
+++ src/customFields/InputField.js
@@ -1,17 +1,21 @@
 import { FormGroup, Input, Label } from "reactstrap";
 
 function InputField(props) {
-  const { field, type, label, disabled } = props;
+  const { field, form, type, label, disabled } = props;
+  const { name } = field;
+  const { errors, touched } = form;
 
   return (
     <FormGroup>
-      <Label for={field.name}>{label}</Label>
+      <Label for={name}>{label}</Label>
       <Input
-        id={field.name}
+        id={name}
         type={type ? type : "text"}
         disabled={disabled}
         {...field}
       />
+
+      {errors[name] && touched[name] ? <p>{errors[name]}</p> : null}
     </FormGroup>
   );
 }
