e4a34191fc21ae796cfa1fd2d875a576d6b8fb06
Son Do <dominhson580@gmail.com>
2023-01-27 17:21:17 +0700

    Populate all fields if in edit mode

--- src/features/photo/pages/AddEdit.js
+++ src/features/photo/pages/AddEdit.js
@@ -1,5 +1,5 @@
 import Banner from "components/Banner";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { useNavigate, useParams } from "react-router-dom";
 import { v4 as uuidv4 } from "uuid";
 import PhotoForm from "../components/PhotoForm";
@@ -7,10 +7,22 @@ import { addPhoto } from "../photoSlice";
 import "./AddEdit.scss";
 
 function Add() {
-  const { id: photoID } = useParams();
-
   const dispatch = useDispatch();
   const navigate = useNavigate();
+
+  const { id: photoID } = useParams();
+
+  const editPhoto = useSelector((state) =>
+    state.photo.find((photo) => photo.id === photoID)
+  );
+
+  const initialValues = photoID
+    ? editPhoto
+    : {
+        title: "",
+        category: null,
+        photo: "",
+      };
 
   const handleSubmit = (values) => {
     const newPhoto = { ...values, id: uuidv4() };
