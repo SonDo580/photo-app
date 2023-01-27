da89c165180a712bbc4ec4cfca848b260a8d5e0b
Son Do <dominhson580@gmail.com>
2023-01-27 16:53:28 +0700

    Implement updatePhoto

--- src/features/photo/photoSlice.js
+++ src/features/photo/photoSlice.js
@@ -93,9 +93,17 @@ const photoSlice = createSlice({
     removePhoto: (state, action) => {
       return state.filter((photo) => photo.id !== action.payload);
     },
+    updatePhoto: (state, action) => {
+      const newPhoto = action.payload;
+      const index = state.findIndex((photo) => photo.id === newPhoto.id);
+
+      if (index >= 0) {
+        state[index] = newPhoto;
+      }
+    },
   },
 });
 
 const { reducer, actions } = photoSlice;
-export const { addPhoto, removePhoto } = actions;
+export const { addPhoto, removePhoto, updatePhoto } = actions;
 export default reducer;
