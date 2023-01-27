3003508883a0b3c6c9691044afe362eab60e3903
Son Do <dominhson580@gmail.com>
2023-01-27 10:47:24 +0700

    Navigate to photo-list page

--- src/features/photo/pages/Add.js
+++ src/features/photo/pages/Add.js
@@ -1,14 +1,17 @@
 import Banner from "components/Banner";
 import { useDispatch } from "react-redux";
+import { useNavigate } from "react-router-dom";
 import PhotoForm from "../components/PhotoForm";
 import { addPhoto } from "../photoSlice";
 import "./Add.scss";
 
 function Add() {
   const dispatch = useDispatch();
+  const navigate = useNavigate();
 
   const handleSubmit = (values) => {
     dispatch(addPhoto(values));
+    navigate("/photos");
   };
 
   return (
