483bec6428fb05098f760177bad75ff300c16fd4
Son Do <dominhson580@gmail.com>
2023-01-27 16:28:43 +0700

    Pass handleRemove function to PhotoCard

--- src/features/photo/components/PhotoList.js
+++ src/features/photo/components/PhotoList.js
@@ -4,11 +4,15 @@ import PhotoCard from "./PhotoCard";
 function PhotoList(props) {
   const { photos } = props;
 
+  const handleRemove = (photoID) => {
+    console.log(photoID);
+  };
+
   return (
     <Row>
       {photos.map((photo) => (
         <Col key={photo.id} xs="12" md="6" lg="3">
-          <PhotoCard photo={photo} />
+          <PhotoCard photo={photo} handleRemove={handleRemove} />
         </Col>
       ))}
     </Row>
