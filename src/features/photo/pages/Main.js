a361124245ff8ed9fc2daa4fabaf893aade95f17
Son Do <dominhson580@gmail.com>
2023-01-27 10:49:28 +0700

    Get list of photos from redux store

--- src/features/photo/pages/Main.js
+++ src/features/photo/pages/Main.js
@@ -1,10 +1,14 @@
-import { Link } from "react-router-dom";
-import { Container } from "reactstrap";
-
 import Banner from "components/Banner";
 import Images from "constants/images";
 
+import { Link } from "react-router-dom";
+import { Container } from "reactstrap";
+import { useSelector } from "react-redux";
+
 function Main() {
+  const photos = useSelector((state) => state.photos);
+  console.log(photos);
+
   return (
     <div className="photo-main">
       <Banner title="Your awesome photos" backgroundUrl={Images.PINK_BG} />
