5e810d3cab17912c5dda0105715b4a8075c5d0cb
Son Do <dominhson580@gmail.com>
2023-01-28 17:25:02 +0700

    Remove comments

--- src/api/axiosClient.js
+++ src/api/axiosClient.js
@@ -11,25 +11,21 @@ const axiosClient = axios.create({
 
 axiosClient.interceptors.request.use(
   (config) => {
-    // Do something before request is sent
     return config;
   },
   (error) => {
-    // Do something with request error
     return Promise.reject(error);
   }
 );
 
 axiosClient.interceptors.response.use(
   (response) => {
-    // Do something with response data
     if (response && response.data) {
       return response.data;
     }
     return response;
   },
   (error) => {
-    // Do something with response error
     return Promise.reject(error);
   }
 );
