b1359e16fed24abdad9a3752966bb717b908063a
Son Do <dominhson580@gmail.com>
2023-01-28 17:11:16 +0700

    Set up config defaults

---
+++ src/api/axiosClient.js
@@ -0,0 +1,10 @@
+import axios from "axios";
+import queryString from "query-string";
+
+const axiosClient = axios.create({
+  baseURL: process.env.REACT_APP_API_URL,
+  headers: {
+    "Content-Type": "application/json",
+  },
+  paramsSerializer: (params) => queryString.stringify(params),
+});
