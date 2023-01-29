a05b4879450a3aa16dc9ee62b30d7fa89cb56e2d
Son Do <dominhson580@gmail.com>
2023-01-29 15:12:49 +0700

    Add Authorization header to each request

--- src/api/axiosClient.js
+++ src/api/axiosClient.js
@@ -9,6 +9,10 @@ const axiosClient = axios.create({
 
 axiosClient.interceptors.request.use(
   (config) => {
+    const token = localStorage.getItem("token");
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
     return config;
   },
   (error) => {
