a9d7a62c7e733ee59e029bff2d463a6a37cb6b1a
Son Do <dominhson580@gmail.com>
2023-01-29 11:09:34 +0700

    Create SignIn page

---
+++ src/features/auth/pages/SignIn/index.js
@@ -0,0 +1,10 @@
+function SignIn() {
+  return (
+    <div className="text-center">
+      <h2>Login Form</h2>
+      <p>or login with social accounts</p>
+    </div>
+  );
+}
+
+export default SignIn;
