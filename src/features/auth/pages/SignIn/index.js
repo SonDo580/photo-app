3c02a98dcddcec28954a3e60aa42c0de98c6a1f4
Son Do <dominhson580@gmail.com>
2023-01-29 12:03:56 +0700

    Use StyledFirebaseAuth with redirect

--- src/features/auth/pages/SignIn/index.js
+++ src/features/auth/pages/SignIn/index.js
@@ -1,8 +1,20 @@
+import firebase from "firebase/compat/app";
+import "firebase/compat/auth";
+import StyledFirebaseAuth from "react-firebaseui/StyledFirebaseAuth";
+
+// Configure FirebaseUI.
+const uiConfig = {
+  signInFlow: "redirect",
+  signInOptions: [firebase.auth.GoogleAuthProvider.PROVIDER_ID],
+  signInSuccessUrl: "/photos",
+};
+
 function SignIn() {
   return (
     <div className="text-center">
       <h2>Login Form</h2>
       <p>or login with social accounts</p>
+      <StyledFirebaseAuth uiConfig={uiConfig} firebaseAuth={firebase.auth()} />
     </div>
   );
 }
